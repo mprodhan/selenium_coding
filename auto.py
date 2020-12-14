2fb6d6f5258de3e94c24dfb9d622c9e7b5384ed2
Mustafa Prodhan <mustafa.prodhan.317@gmail.com>
2020-12-13 22:23:46 -0500

    performed a button check for submission on a form.

--- auto.py
+++ auto.py
@@ -17,3 +17,16 @@ assert 'Selenium Easy' in chrome_browser.title
 
 button = chrome_browser.find_element_by_class_name('btn-default')
 print(button.get_attribute('innerHTML'))
+
+assert 'Show Message' in chrome_browser.page_source
+
+user_message = chrome_browser.find_element_by_id('user-message')
+user_button = chrome_browser.find_element_by_css_selector('.btn')
+print(user_button)
+user_message.clear()
+user_message.send_keys('Mufassa!!!')
+button.click()
+output_message = chrome_browser.find_element_by_id('display')
+assert 'Mufassa!!!' in output_message.get_attribute('innerHTML')
+
+chrome_browser.close()
